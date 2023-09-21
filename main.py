a5e692ccffad1ada4a496d2d95dbc80ec43aeb15
David J Mallick <davidjmallick@gmail.com>
2023-09-21 16:57:02 -0700

    fix + refined X, Y form

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def form_X_and_Y(rs):
     sample_rs = rs.head(100)
     close = sample_rs['Close']
     X, Y = [], []
-    for i in range(len(sample_rs) - N_MA - 1):
-        x = rs.iloc[i:i + N_MA + 1]
-        y = rs.iloc[i + N_MA + 1]
+    for i in range(len(close) - N_MA - 1):
+        x = close.iloc[i:i + N_MA + 1]
+        y = close.iloc[i + N_MA + 1]
         X.append(x)
         Y.append(y)
     return X, Y

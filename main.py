ef2a86fa1c7f24dda358363e043e94c90f1d0b70
David J Mallick <davidjmallick@gmail.com>
2024-08-05 12:52:40 -0600

    enable run of main.py from jupyter notebook. (for instance, update ipython kernel to python 3)

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+
+# ToDo: Put print output into... dataframe?? Table
+# ToDo: Debug iloc in main.py??
+
+
 import pandas as pd
 
 
 
 def read():
-    return pd.read_csv("stock_market_data-AAPL.csv")
+    return pd.read_csv("stock_market_data-AAPL.csv", parse_dates=["Date"])
 
 
 """
-@return X: List
-@return Y: List
+@input rs:
+@input n_ma:
+@return X, Y: Tuple of Lists of Series
+    X: List of Series, where each Series is >1 element
+    Y: List of Series, where each Series is 1 element
 """
 def form_Xs_and_Ys(rs, n_ma):
     sample_rs = rs.head(100)
@@ -42,23 +50,26 @@ if __name__ == "__main__":
     rs = read()
     print("stock_market_data-AAPL.csv read")
     print(rs.head())
+    print("\n\n")
 
     n_mas = [10, 20, 30]
     XYs = {}
     for n_ma in n_mas:
         X, Y = form_Xs_and_Ys(rs, n_ma)
         XYs[n_ma] = (X, Y)
-    print("X formed")
-    print(X)
-    print("Y formed")
-    print(Y)
+    print("X(%d) formed. Head:" % n_mas[0])
+    print(XYs[n_mas[0]][0][:1])
+    print("Y(%d) formed. Head:" % n_mas[0])
+    print(XYs[n_mas[0]][1][:1])
+    print("\n\n")
 
     XYs_1 = {}
     for n_ma in XYs.keys():
         Y_prime = calculate_Y_prime(XYs[n_ma][0])
         XYs_1[n_ma] = (XYs[n_ma][0], XYs[n_ma][1], Y_prime)
         print("Y_prime:")
-        print(Y_prime)
+        print(Y_prime[:10])
+    print("\n\n")
 
     for k, v in XYs_1.items():
         Y_prime = v[2]
